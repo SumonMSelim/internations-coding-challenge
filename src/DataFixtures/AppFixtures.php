3c9ca85f0cbb70fd8f9bc008a76c6af70f6fd50a
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2018-12-21 03:10:18 +0800

    Admin user seeder added. Run: bin/console doctrine:fixtures:load  --append

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private $passwordEncoder;
 
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setUsername('admin');
+        $password = $this->passwordEncoder->encodePassword($admin, 'secret123');
+        $admin->setPassword($password);
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
         $manager->flush();
     }
 }
